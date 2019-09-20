3ec655cad1e3454bfdc35f3a5e96a569cbf99000
vinhdn <vinhdn07@gmail.com>
2019-09-20 09:23:45 +0700

    Upgrade androidX

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/vinh/Code/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/vinh/Code/AppTongHop/BaoDay"
-export "FLUTTER_TARGET=/Users/vinh/Code/AppTongHop/BaoDay/lib/main.dart"
+export "FLUTTER_ROOT=E:\Code\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Code\Flutter\GGNews"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/vinh/Code/Flutter/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=E:\Code\Flutter\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
